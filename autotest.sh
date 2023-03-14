55cb03cf4eeb880437f1608dfac8988d15749376
Alyona Teyber <Ivanova.alyona5@gmail.com>
2023-03-14 13:03:35 -0700

    Update autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -8,8 +8,8 @@ FILES=
 
 for OPENFIDO_INPUT in $(find $PWD/autotest -name 'input_*' -print); do
     echo "Processing $OPENFIDO_INPUT..."
-#     export OPENFIDO_INPUT
-#     export OPENFIDO_OUTPUT=${OPENFIDO_INPUT/autotest\/input_/autotest\/output_}
+    export OPENFIDO_INPUT
+    # export OPENFIDO_OUTPUT=${OPENFIDO_INPUT/autotest\/input_/autotest\/output_}
 #     mkdir -p $OPENFIDO_OUTPUT
 #     rm -rf $OPENFIDO_OUTPUT/{*,.??*}
 #     if ! bash openfido.sh 1>$OPENFIDO_OUTPUT/stdout 2>$OPENFIDO_OUTPUT/stderr; then
