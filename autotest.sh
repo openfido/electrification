cadfeabbd4e370f472cb7fc4c677fc998050df0a
Alyona Teyber <Ivanova.alyona5@gmail.com>
2023-03-14 16:35:34 -0700

    adding back artifacts code

--- autotest.sh
+++ autotest.sh
@@ -20,13 +20,13 @@ for OPENFIDO_INPUT in $(find $PWD/autotest -name 'input_*' -print); do
     TESTED=$(($TESTED+1))
 done
 
-# echo "$TESTED tests completed"
-# echo "$FAILED tests failed"
-# if [ $FAILED -gt 0 ]; then
-#     tar cfz autotest-errors.tar.gz $FILES
-#     echo "Failure artifacts saved to autotest-errors.tar.gz"
-#     exit 1
-# else
-#     echo "Success"
-#     exit 0
-# fi
+echo "$TESTED tests completed"
+echo "$FAILED tests failed"
+if [ $FAILED -gt 0 ]; then
+    tar cfz autotest-errors.tar.gz $FILES
+    echo "Failure artifacts saved to autotest-errors.tar.gz"
+    exit 1
+else
+    echo "Success"
+    exit 0
+fi
