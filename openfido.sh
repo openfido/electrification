191501b1eb972f058d619ef373b0de892c1c81ec
Alyona Teyber <Ivanova.alyona5@gmail.com>
2023-03-15 15:48:10 -0700

    Further edits to input_3 autotest

--- openfido.sh
+++ openfido.sh
@@ -32,7 +32,7 @@ elif [ ! -f "$OPENFIDO_INPUT/gridlabd.rc" ]; then
     fi
 else
     if [ -f "$OPENFIDO_INPUT/config.csv" ]; then 
-        echo "#input "config.csv" -f "config" -t "config"" > "$OPENFIDO_INPUT/config.glm"
+        echo "#input "config.csv" -f "config" -t "config"" > "$OPENFIDO_INPUT/config-csv-convert.glm"
     fi
     OPTIONS=$(cd $OPENFIDO_INPUT ; cat gridlabd.rc | tr '\n' ' ')
 fi
